c4d2510bfbdb6400a6be51773e94df034cf4c0cd
Matthew John Lapehn <mlapehn@gmail.com>
2016-07-27 18:58:30 -0600

    Good check point, added validates

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,7 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+	validates :first_name, presence: true, length: {maximum: 255}
+	validates :last_name, presence: true, length: {maximum: 255}
+	validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: { with: /@/}
 
   before_save :normalize_phone_number
 
